a8ef3b26223704d2092fe6041ede69b2a9b58f59
parkerjustice <you@example.com>
2022-03-16 18:20:53 -0400

    added js

--- public/js/idbs.js
+++ public/js/idbs.js
@@ -5,7 +5,7 @@ request.onerror = function(event) {
     console.log(event.target.errorCode)
 };
 
-function recordSave(record) {
+function saveRecord(record) {
     const trans = db.transaction(['item_progression'], 'redo');
 
     const budgetObjStore = trans.objectStore('item_progression');
@@ -20,10 +20,46 @@ request.onsuccess = function(event) {
     db = event.target.result;
 
     if (navigator.online) {
-        uploadBudget();
+        bringUp();
     }
 };
 
 request.onerror = function(event) {
     console.log(event.target.errorCode)
 };
+function bringUp() {
+    const trans = db.transaction(['item_progression'], 'redo');
+
+    const budgetObjStore = trans.objectStore('item_progression');
+
+    const getAll = budgetObjStore.getAll();
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const trans = db.transaction(['item_progression'], 'redo');
+
+                    const budgetObjStore = trans.objectStore('item_progression');
+
+                    budgetObjStore.clear();
+
+                    alert('All Items have been entered')
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+window.addEventListener('online', bringUp);
