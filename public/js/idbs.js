2c8f384d84764d8312dbee30ecf97ab4be25d32a
parkerjustice <you@example.com>
2022-03-16 18:25:50 -0400

    fixed js

--- public/js/idbs.js
+++ public/js/idbs.js
@@ -1,29 +1,25 @@
 let db;
 const request = indexedDB.open("SaveYourMoney", 1);
-
 request.onerror = function(event) {
     console.log(event.target.errorCode)
 };
 
 function saveRecord(record) {
     const funding = db.transaction(['item_progression'], 'redo');
-
     const storing = funding.objectStore('item_progression');
     storing.add(record)
 }
 request.onupgradeneeded = function(event) {
-    const db = event.target.result;
-    db.createObjectStore('item_progression', { autoIncrement: true });
+const db = event.target.result;
+db.createObjectStore('item_progression', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
     db = event.target.result;
-
     if (navigator.online) {
         bringUp();
     }
 };
-
 request.onerror = function(event) {
     console.log(event.target.errorCode)
 };
