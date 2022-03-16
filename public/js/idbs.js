8c2ef2422b2452c9688ec0eb3b19fc52a14101d5
parkerjustice <you@example.com>
2022-03-16 18:18:13 -0400

    added js

--- public/js/idbs.js
+++ public/js/idbs.js
@@ -1,2 +1,29 @@
 let db;
-const request = indexedDB.open("budget", 1);
+const request = indexedDB.open("SaveYourMoney", 1);
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+};
+
+function recordSave(record) {
+    const trans = db.transaction(['item_progression'], 'redo');
+
+    const budgetObjStore = trans.objectStore('item_progression');
+    budgetObjStore.add(record)
+}
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('item_progression', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.online) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+};
