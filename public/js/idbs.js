81eae4935d3bed82752e8bed969758d2d261e6d8
parkerjustice <you@example.com>
2022-03-19 15:02:18 -0400

    added js

--- public/js/idbs.js
+++ public/js/idbs.js
@@ -1,6 +1,18 @@
-let db;
+let DB;
+const storeName = "store";
 const request = indexedDB.open("SaveYourMoney", 1);
+//onupgradeneeded or onsuccess? 
+request.onuppgradeneeded = function(event) {
+    const DB = event.target.result; 
+console.log(event);
+    DB.createObjectStore("store", { autoIncrement: true});
+};
 
 request.onsuccess = function(event) {
-    
+    console.log("Successful", event);
+    DB = event.target.result;
+};
+
+request.onerror = function(event){
+    console.log(event.target.error);
 }
